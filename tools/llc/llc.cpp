12b4e85e0f506e8abae2dc46198fc810badbc5ae
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2015-11-16 00:28:14 +0100

    wip

--- tools/llc/llc.cpp
+++ tools/llc/llc.cpp
@@ -199,6 +199,7 @@ int main(int argc, char **argv) {
   initializeLoopStrengthReducePass(*Registry);
   initializeLowerIntrinsicsPass(*Registry);
   initializeUnreachableBlockElimPass(*Registry);
+  initializeInstrumentation(*Registry);
 
   // Register the target printer for --version.
   cl::AddExtraVersionPrinter(TargetRegistry::printRegisteredTargetsForVersion);
